d5cd2be305bfed9be6bbfc22b3cb1cd7fc9a80a5
tuanbi97 <tuanbi97@gmail.com>
2018-10-25 13:06:30 -0500

    png2jpg

---
+++ PNG2JPG.py
@@ -0,0 +1,15 @@
+from PIL import Image
+import os
+import glob
+
+dirs = os.listdir('./targettest_70')
+print(dirs)
+
+for i in range(0, len(dirs)):
+	image_folder = dirs[i]
+	image_path = glob.glob('./targettest_70/' + image_folders +'/*.PNG')
+	im = Image.open(image_path)
+	im.load()
+	background = Image.new("RGB", png.size, (255, 255, 255))
+	background.paste(im, mask=im.split()[3]) # 3 is the alpha channel
+	background.save(image_path[0:-4] + '.jpg')
