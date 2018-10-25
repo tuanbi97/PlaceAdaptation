c94c0384af947881009165ecdb7c1cb3d93f6f1d
tuanbi97 <tuanbi97@gmail.com>
2018-10-25 13:08:17 -0500

    fix png2jpg

--- PNG2JPG.py
+++ PNG2JPG.py
@@ -3,11 +3,12 @@ import os
 import glob
 
 dirs = os.listdir('./targettest_70')
+print(len(dirs))
 print(dirs)
 
 for i in range(0, len(dirs)):
 	image_folder = dirs[i]
-	image_path = glob.glob('./targettest_70/' + image_folders +'/*.PNG')
+	image_path = glob.glob('./targettest_70/' + image_folder +'/*.PNG')
 	im = Image.open(image_path)
 	im.load()
 	background = Image.new("RGB", png.size, (255, 255, 255))
