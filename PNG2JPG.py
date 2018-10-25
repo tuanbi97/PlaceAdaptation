05bc3f133a68cecb22eafa1bb1d1bc58c090d9b5
tuanbi97 <tuanbi97@gmail.com>
2018-10-25 13:10:21 -0500

    a

--- PNG2JPG.py
+++ PNG2JPG.py
@@ -8,10 +8,11 @@ print(dirs)
 
 for i in range(0, len(dirs)):
 	image_folder = dirs[i]
-	image_path = glob.glob('./targettest_70/' + image_folder +'/*.PNG')
-	print(image_path)
-	im = Image.open(image_path)
-	im.load()
-	background = Image.new("RGB", png.size, (255, 255, 255))
-	background.paste(im, mask=im.split()[3]) # 3 is the alpha channel
-	background.save(image_path[0:-4] + '.jpg')
+	image_paths = glob.glob('./targettest_70/' + image_folder +'/*.PNG')
+	for image_path in image_paths:
+		print(image_path)
+		im = Image.open(image_path)
+		im.load()
+		background = Image.new("RGB", png.size, (255, 255, 255))
+		background.paste(im, mask=im.split()[3]) # 3 is the alpha channel
+		background.save(image_path[0:-4] + '.jpg')
